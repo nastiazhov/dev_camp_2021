4ca4290d119567969eed4f04c7082f177e2eb4e7
Nastia Zhovtobriukh <nastyazhov100@gmail.com>
2022-01-10 20:40:55 +0200

    api routes

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const config = require('./services/config');
+
+const usersRoutes = require('./routes/users');
+const postsRoutes = require('./routes/posts');
+const likesRoutes = require('./routes/likes');
+const commentsRoutes = require('./routes/comments');
 
 const app = express();
+const port = config.appPort;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/users', usersRoutes);
+app.use('/posts', postsRoutes);
+app.use('/likes', likesRoutes);
+app.use('/comments', commentsRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-console.log("fv")
 
-app.listen(process.env.APP_PORT);
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
