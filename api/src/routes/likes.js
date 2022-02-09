4ee888c06a3a2546edc463d6d28aa453275150bc
Anastasiia Zhovtobriukh <nastyazhov100@gmail.com>
2022-02-09 20:09:21 +0200

    api middlewares

--- api/src/routes/likes.js
+++ api/src/routes/likes.js
@@ -1,47 +1,28 @@
 const router = require('express').Router();
 const serviceLikes = require('../services/likes');
+const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
 
 module.exports = router;
 
-router.get('/', async (req, res) => {
-    try {
-        res.status(200).json(await serviceLikes.getAllLikes());
-    } catch (err) {
-        res.send(err);
-    }
-});
+router.get('/', asyncErrorHandler(async (req, res) => {
+    res.status(200).json(await serviceLikes.getAllLikes());
+}));
 
-router.get('/:UserID/:PostID', async (req, res) => {
-    try {
-        res.status(200).json(await serviceLikes.getLikesByUserIdPostId(req.params.userId, req.params.postId));
-    } catch (err) {
-        res.send(err);
-    }
-});
+router.get('/:UserID/:PostID', asyncErrorHandler(async (req, res) => {
+    res.status(200).json(await serviceLikes.getLikesByUserIdPostId(req.params.userId, req.params.postId));
+}));
 
-router.post('/', async (req, res) => {
-    try {
-        await serviceLikes.createNewLike(req.body);
-        res.status(200).send('New like has been created');
-    } catch (err) {
-        res.send(err);
-    }
-});
+router.post('/', asyncErrorHandler(async (req, res) => {
+    await serviceLikes.createNewLike(req.body);
+    res.status(200).send('New like has been created');
+}));
 
-router.put('/:UserID/:PostID', async (req, res) => {
-    try {
-        await serviceLikes.updateLikeUserIdPostId(req.params.userId, req.params.postId, req.body);
-        res.status(200).send('Like was updated');
-    } catch (err) {
-        res.send(err);
-    }
-});
+router.put('/:UserID/:PostID', asyncErrorHandler(async (req, res) => {
+    await serviceLikes.updateLikeUserIdPostId(req.params.userId, req.params.postId, req.body);
+    res.status(200).send('Like was updated');
+}));
 
-router.delete('/:UserID/:PostID', async (req, res) => {
-    try {
-        await serviceLikes.deleteLikeUserIdPostId(req.params.userId, req.params.postId);
-        res.status(200).send('Like was deleted');
-    } catch (err) {
-        res.send(err);
-    }
-});
+router.delete('/:UserID/:PostID', asyncErrorHandler(async (req, res) => {
+    await serviceLikes.deleteLikeUserIdPostId(req.params.userId, req.params.postId);
+    res.status(200).send('Like was deleted');
+}));
