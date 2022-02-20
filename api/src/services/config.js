016a95f3deb7365f6ccddf90a4af12a3960c3ae2
Anastasiia Zhovtobriukh <nastyazhov100@gmail.com>
2022-02-20 20:13:05 +0200

    api google auth

--- api/src/services/config.js
+++ api/src/services/config.js
@@ -4,4 +4,8 @@ module.exports = {
     dbUser: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD,
     dbDatabase: process.env.DB_DATABASE,
+    salt: process.env.SALT,
+    appKey: process.env.APP_KEY,
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
 };
--- api/src/services/users.js
+++ api/src/services/users.js
@@ -1,12 +1,18 @@
 const db = require('../db');
+const passwordHasher = require('../passwordHasher');
+const config = require('../config');
 
 module.exports = {
     getAllUsers: async () => db.select().from('Users').orderBy('UserId'),
-    getUserById: async (id) => db.select().from('Users').where('UserId', id),
+    getUserById: async (id) => db.select().first().from('Users').where('UserId', id),
     getUserPosts: async (id) => db.select().from('Posts').where('UserId', id),
     getUserAvatar: async (id) => db.select('avatar').from('Users').where('UserId', id),
     uploadUserAvatar: async (id, filepath) => db.update('avatar', filepath).from('Users').where('UserId', id),
-    createNewUser: async (body) => db.insert(body).into('Users'),
+    createNewUser: async (body) => {
+        if (body.password) body.password = passwordHasher(body.password, config.salt);
+        return db.insert(body).into('Users');
+      },
+    checkPassword: (plainPassword, hash) => hash === passwordHasher(plainPassword, config.salt),
     updateUser: async (id, body) => db.select().from('Users').where('UserId', id).update(body),
     deleteUser: async (id) => db.select().from('Users').where('UserId', id).del(),
 };
